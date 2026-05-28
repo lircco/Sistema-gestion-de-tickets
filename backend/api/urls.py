02974ac6bdccdfa625ee00c727ff15a86f39e0f6
agustin-arg <agustinalbertoleiva@gmail.com>
2026-05-28 12:40:01 -0300

    feat: registrar endpoints de áreas y categorías

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TicketViewSet, LoginView, LogoutView, UserView
+from .views import TicketViewSet, LoginView, LogoutView, UserView, AreaViewSet, CategoriaViewSet
 
 router = DefaultRouter()
 router.register(r'tickets', TicketViewSet, basename='ticket')
+router.register(r'areas', AreaViewSet, basename='area')
+router.register(r'categorias', CategoriaViewSet, basename='categoria')
 
 urlpatterns = [
     path('', include(router.urls)),
