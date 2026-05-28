2c55c2b1b4956981b4a690be18df0ce1fe71071f
agustin-arg <agustinalbertoleiva@gmail.com>
2026-05-28 12:35:49 -0300

    feat: registrar endpoints de login, logout y me

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TicketViewSet
+from .views import TicketViewSet, LoginView, LogoutView, UserView
 
 router = DefaultRouter()
 router.register(r'tickets', TicketViewSet, basename='ticket')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('me/', UserView.as_view(), name='me'),
 ]
